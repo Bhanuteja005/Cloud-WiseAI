f563c98c1a9c557f285c2968a40b835b765c5f21
Bhanuteja005 <pashikantibhanuteja@gmail.com>
2024-03-05 20:25:31 +0530

    bhanu

--- js/scripts.js
+++ js/scripts.js
@@ -8,45 +8,73 @@ const firebaseConfig = {
     appId: "1:819738537672:web:f9e9286160bb3454d67ffd"
   };
   
-  // initialize firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  // reference your database
-  var contactFormDB = firebase.database().ref("cloudForm");
-  
-  document.getElementById("cloudForm").addEventListener("submit", submitForm);
-  
-  function submitForm(e) {
-    e.preventDefault();
-  
-    var name = getElementVal("name");
-    var emailid = getElementVal("emailid");
-    var msgContent = getElementVal("msgContent");
-  
-    saveMessages(name, emailid, msgContent);
-  
-    //   enable alert
-    document.querySelector(".alert").style.display = "block";
-  
-    //   remove the alert
-    setTimeout(() => {
-      document.querySelector(".alert").style.display = "none";
-    }, 3000);
-  
-    //   reset the form
-    document.getElementById("cloudForm").reset();
-  }
-  
-  const saveMessages = (name, emailid, msgContent) => {
-    var newContactForm = contactFormDB.push();
-  
-    newContactForm.set({
-      name: name,
-      emailid: emailid,
-      msgContent: msgContent,
-    });
-  };
-  
-  const getElementVal = (id) => {
-    return document.getElementById(id).value;
-  };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+ // Reference your database
+ var contactFormDB = firebase.database().ref("cloudForm");
+
+ // Initialize Firebase storage
+ var storage = firebase.storage();
+
+ document.getElementById("cloudForm").addEventListener("submit", submitForm);
+
+ function submitForm(e) {
+   e.preventDefault();
+
+   var name = getElementVal("name");
+   var emailid = getElementVal("emailid");
+   var file = document.getElementById("file").files[0];
+
+   if (file) {
+     // Create a storage reference
+     var storageRef = storage.ref("messages/" + new Date().getTime() + "_" + file.name);
+
+     // Upload the file to Firebase storage
+     var task = storageRef.put(file);
+
+     // Update progress bar
+     task.on("state_changed", function(snapshot) {
+       var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+       console.log("Upload is " + percentage + "% done");
+     }, function(error) {
+       // Handle error
+       console.error(error);
+     }, function() {
+       // Handle successful upload
+       task.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+         // Save the download URL and other form data to the realtime database
+         saveMessages(name, emailid, file.name, downloadURL);
+       });
+     });
+   } else {
+     // If no file is selected, save other form data without file
+     saveMessages(name, emailid, null, null);
+   }
+
+   // Enable alert
+   document.querySelector(".alert").style.display = "block";
+
+   // Remove the alert
+   setTimeout(() => {
+     document.querySelector(".alert").style.display = "none";
+   }, 3000);
+
+   // Reset the form
+   document.getElementById("cloudForm").reset();
+ }
+
+ const saveMessages = (name, emailid, fileID, downloadURL = null) => {
+   var newContactForm = contactFormDB.push();
+
+   newContactForm.set({
+     name: name,
+     emailid: emailid,
+     fileID: fileID,
+     downloadURL: downloadURL
+   });
+ };
+
+ const getElementVal = (id) => {
+   return document.getElementById(id).value;
+ };
